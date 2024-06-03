bb0de6bfc90831131a200fa80832bb2aae28e5c2
Slava Khripushin <slava.cpa@yandex.ru>
2024-06-03 20:50:11 +0700

    update

--- src/js/main.js
+++ src/js/main.js
@@ -74,4 +74,58 @@ document.addEventListener('DOMContentLoaded', function () {
     nav.classList.toggle('show');
     burger.classList.toggle('show');
   });
+
+  const cc = initCookieConsent();
+  cc.run({
+    gui_options: {
+      consent_modal: { layout: 'box', position: 'bottom right', transition: 'slide', swap_buttons: !0 },
+      settings_modal: { layout: 'box', position: 'left', transition: 'slide' },
+    },
+    current_lang: 'en',
+    autoclear_cookies: !0,
+    page_scripts: !0,
+    onFirstAction: function (e, t) {},
+    onAccept: function (e) {},
+    onChange: function (e, t) {},
+    languages: {
+      en: {
+        consent_modal: {
+          title: 'We use cookies!',
+          description:
+            'Hi, this website uses essential cookies to ensure its proper operation and tracking cookies to understand how you interact with it. The latter will be set only after consent. <button type="button" data-cc="c-settings" class="cc-link">Let me choose</button>',
+          primary_btn: { text: 'Accept all', role: 'accept_all' },
+          secondary_btn: { text: 'Reject all', role: 'accept_necessary' },
+        },
+        settings_modal: {
+          title: 'Cookie preferences',
+          save_settings_btn: 'Save settings',
+          accept_all_btn: 'Accept all',
+          reject_all_btn: 'Reject all',
+          close_btn_label: 'Close',
+          cookie_table_headers: [{ col1: 'Name' }, { col2: 'Domain' }, { col3: 'Expiration' }, { col4: 'Description' }],
+          blocks: [
+            {
+              title: 'Cookie usage рџ“ў',
+              description:
+                'I use cookies to ensure the basic functionalities of the website and to enhance your online experience. You can choose for each category to opt-in/out whenever you want. For more details relative to cookies and other sensitive data, please read the full <a href="/privacy.html" class="cc-link">privacy policy</a>.',
+            },
+            {
+              title: 'Performance and Analytics cookies',
+              description: 'These cookies allow the website to remember the choices you have made in the past',
+              toggle: { value: 'analytics', enabled: !0, readonly: !1 },
+              cookie_table: [
+                { col1: '^_ga', col2: 'google.com', col3: '2 years', col4: 'Google analytics ...', is_regex: !0 },
+                { col1: '_gid', col2: 'google.com', col3: '1 day', col4: 'Google analytics ...' },
+              ],
+            },
+            {
+              title: 'More information',
+              description:
+                'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="/contact.html">contact us</a>.',
+            },
+          ],
+        },
+      },
+    },
+  });
 });
